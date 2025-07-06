1a2071cdce6d5f574f2590705100fcfc9065e916
Callixta Fidelia C <callixta124@gmail.com>
2025-07-06 13:11:09 -0700

    change config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   eslint: {
+    ignoreDuringBuilds: true, // This disables ESLint during next build
+  },
  output : 'export',
   images : {
     unoptimized : true,
