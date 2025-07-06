fead9b737e10196fc5a6fc5ec763827758203175
Callixta Fidelia C <callixta124@gmail.com>
2025-07-06 13:52:48 -0700

    add logo 4

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-   eslint: {
-    ignoreDuringBuilds: true, // This disables ESLint during next build
-  },
- output : 'export',
-  images : {
-    unoptimized : true,
-  }
-};
+const isProd = process.env.NODE_ENV === "production";
+const repo = "/rebirdth";
 
+const nextConfig: NextConfig = {
+  basePath: isProd ? repo : "",
+  assetPrefix: isProd ? `${repo}/` : "",
+  output: "export",
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+};
 
 export default nextConfig;
